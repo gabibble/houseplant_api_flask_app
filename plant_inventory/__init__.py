ca67c25c30d3996106c5f7f65e0ea8ef05b558cf
gabibble <juliagieb@gmail.com>
2022-09-07 18:43:08 -0700

    day2

--- plant_inventory/__init__.py
+++ plant_inventory/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from config import Config
 from .site.routes import site
 from .authentication.routes import auth
+from config import Config
+
 # TODO  api
+
+from flask_migrate import Migrate
+from .models import db as root_db, login_manager, ma
 
 app = Flask(__name__)
 
@@ -11,3 +15,12 @@ app.register_blueprint(auth)
 # TODO  api
 
 app.config.from_object(Config)
+
+root_db.init_app(app)
+
+migrate = Migrate(app, root_db)
+
+login_manager.init_app(app)
+login_manager.login_view = 'auth.signin'
+
+ma.init_app(app)
---
+++ plant_inventory/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Email
+
+class UserLoginForm(FlaskForm):
+    first_name = StringField('First name', validators = [InputRequired()])
+    last_name = StringField('First name')
+    email = StringField('Email', validators = [InputRequired(), Email()])
+    password = PasswordField('Password', validators = [InputRequired()])
+    submit_button = SubmitField()
